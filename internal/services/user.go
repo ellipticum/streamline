7f3a15343edffe3c4d937d3ba05e4ab8301cf4f5
ellipticum <ellipticum@outlook.com>
2024-01-23 02:14:24 +0600

    fix: fix the receipt logic

--- internal/services/user.go
+++ internal/services/user.go
@@ -3,8 +3,11 @@ package services
 import (
 	"context"
 	"github.com/ellipticum/streamline/internal/dto"
+	"github.com/ellipticum/streamline/internal/models"
 	"github.com/ellipticum/streamline/pkg/storage"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"log"
 	"time"
 )
 
@@ -12,18 +15,38 @@ type User struct{}
 
 var delay = 10 * time.Second
 
-func (user User) Get() (*mongo.Cursor, error) {
+func (user User) Get() ([]models.User, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), delay)
 
 	defer cancel()
 
-	result, err := storage.Client.Database("streamline").Collection("users").Find(ctx, nil)
+	cursor, err := storage.Client.Database("streamline").Collection("users").Find(ctx, bson.D{})
 
 	if err != nil {
 		return nil, err
 	}
 
-	return result, nil
+	defer cursor.Close(ctx)
+
+	var results []models.User
+
+	for cursor.Next(ctx) {
+		var elem models.User
+
+		err := cursor.Decode(&elem)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		results = append(results, elem)
+	}
+
+	if err := cursor.Err(); err != nil {
+		return nil, err
+	}
+
+	return results, nil
 }
 
 func (user User) Create(data dto.UserCreateDTO) (*mongo.InsertOneResult, error) {
